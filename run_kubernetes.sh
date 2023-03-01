3208102377b112460b5203b88adf22882d22eba7
EC2 Default User <ec2-user@ip-172-31-21-106.ec2.internal>
2023-03-01 09:20:03 +0000

    updated kubernates_out

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mithragk22/service-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run service-api --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
+
+# Wait till container is running
+sleep 40
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward service-api 8000:80
